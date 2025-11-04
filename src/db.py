c03814cfaeb867e8420744336a1e1d2627c64816
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-11-04 20:29:19 +0530

    Connected User table to Post table

--- src/db.py
+++ src/db.py
@@ -26,11 +26,13 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 class Post(Base):
     __tablename__ = 'posts'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
     file_name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.now(ZoneInfo('Asia/Kolkata')))
+    user = relationship('User', back_populates='posts')
 
 
 engine = create_async_engine(DB_URL)
