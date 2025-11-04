44529d715619145d5839d374341dd22de008e02f
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-11-04 11:44:38 +0530

    Added a Pydantic class for Post table

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from uuid import UUID
+from datetime import datetime
 
-class PostCreate(BaseModel):
-    title: str
-    content: str
+class PostResponse(BaseModel):
+    id: UUID
+    caption: str | None = None
+    url: str
+    file_type: str
+    file_name: str
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True, arbitrary_types_allowed=True)
