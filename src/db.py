c5d13b78e656b37b17ecc13e9526d2fb3c43680c
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-11-04 10:50:31 +0530

    Async function for database sessions

--- src/db.py
+++ src/db.py
@@ -29,3 +29,7 @@ async_sesson_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def create_db_and_tables():
     async with engine.begin() as conn:
         await conn.run_sync(DeclarativeBase.metadata.create_all)
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_sesson_maker() as sess:
+        yield sess
