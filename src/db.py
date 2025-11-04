e05c657e73ff6717b70c83f0959802530e00d597
LordZeusIsBack <anubhavsharma5645@gmail.com>
2025-11-04 10:58:18 +0530

    Corrected Declarative Base

--- src/db.py
+++ src/db.py
@@ -13,7 +13,12 @@ from dotenv import load_dotenv
 load_dotenv()
 DB_URL = os.getenv('DATABASE_URL')
 
-class Post(DeclarativeBase):
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Post(Base):
     __tablename__ = 'posts'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     caption = Column(Text)
@@ -28,7 +33,7 @@ async_sesson_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_sesson_maker() as sess:
